ee165b5703d9b0faa9d121cf9a7640f9b5cb1146
Etienne Julien <etienne.etiennejulien@gmail.com>
2024-10-06 17:14:35 +0200

    fin entrainement discriminateur

--- GAN.py
+++ GAN.py
@@ -19,6 +19,7 @@ from keras import layers
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
 
+batchsize=1 #Nb d'echantillons traité en même temps
 
 #1) Creation du réseau Discriminateur
 #Voir algo NumberDetection pour plus de details sur comment il fonctionne
@@ -51,7 +52,6 @@ class CNN(nn.Module):
         x = self.maxpooling2(x)
         x = x.view(-1, 64 * 4 * 4)
         x = F.leaky_relu(self.fcl1(x))
-        print(torch.sigmoid(self.fcl2(x)))
         x = torch.sigmoid(self.fcl2(x))
         
         return x
@@ -75,65 +75,83 @@ class Generator(nn.Module):
     def forward(self,x):
           x=F.leaky_relu(self.deconv1(x))
           y=F.leaky_relu(self.deconv2(x))
-          z=F.leaky_relu(self.deconv3(y))
-          print(x.size())
-          print(y.size())
-          print(z.size())
-          return x,y,z
+          z=torch.tanh(F.leaky_relu(self.deconv3(y))) # fonction d'activation tangente hyperbolique : centralise les valeurs entre -1 et 1, facilite l'apprentissage.
+          return z
         
 
-gen=Generator()  
-noise = torch.randn(1,100,1,1) # 64 échantillons, 100 dimensions, 1x1 (h, w)
-print(noise.size())
-result,result2,result3 = gen(noise)  
+# gen=Generator()  
+# noise = torch.randn(batchsize,100,1,1) # 64 échantillons, 100 dimensions, 1x1 (h, w)
+# print(noise.size())
+# result,result2,result3 = gen(noise)  
 
-#affichage
-fig, axs = plt.subplots(1, 6, figsize=(8, 12))
-fig, axs2 = plt.subplots(1, 6, figsize=(8, 12))
-fig, axs3 = plt.subplots(1, 1, figsize=(8, 12))
-for i in range(6):
-    axs[i].imshow(result[0][i].detach().numpy(), cmap='gray')
-    axs[i].set_title(f'Image')
-    axs[i].axis('off')
-for i in range(6):
-    axs2[i].imshow(result2[0][i].detach().numpy(), cmap='gray')
-    axs2[i].set_title(f'Image')
-    axs2[i].axis('off')
-
-
-    axs3.imshow(result3[0][0].detach().numpy(), cmap='gray')
-    axs3.set_title(f'Image')
-    axs3.axis('off')
-
-gen=Generator()  
-noise = torch.randn(1,100,1,1) # 64 échantillons, 100 dimensions, 1x1 (h, w)
-print(noise.size())
-result,result2,result3 = gen(noise)  
-
-#affichage
-fig, axs = plt.subplots(1, 6, figsize=(8, 12))
-fig, axs2 = plt.subplots(1, 6, figsize=(8, 12))
-fig, axs3 = plt.subplots(1, 1, figsize=(8, 12))
-for i in range(6):
-    axs[i].imshow(result[0][i].detach().numpy(), cmap='gray')
-    axs[i].set_title(f'Image')
-    axs[i].axis('off')
-for i in range(6):
-    axs2[i].imshow(result2[0][i].detach().numpy(), cmap='gray')
-    axs2[i].set_title(f'Image')
-    axs2[i].axis('off')
+# #affichage
+# fig, axs = plt.subplots(1, 6, figsize=(8, 12))
+# fig, axs2 = plt.subplots(1, 6, figsize=(8, 12))
+# fig, axs3 = plt.subplots(1, 1, figsize=(8, 12))
+# for i in range(6):
+#     axs[i].imshow(result[0][i].detach().numpy(), cmap='gray')
+#     axs[i].set_title(f'Image')
+#     axs[i].axis('off')
+# for i in range(6):
+#     axs2[i].imshow(result2[0][i].detach().numpy(), cmap='gray')
+#     axs2[i].set_title(f'Image')
+#     axs2[i].axis('off')
 
 
-    axs3.imshow(result3[0][0].detach().numpy(), cmap='gray')
-    axs3.set_title(f'Image')
-    axs3.axis('off')
+#     axs3.imshow(result3[0][0].detach().numpy(), cmap='gray')
+#     axs3.set_title(f'Image')
+#     axs3.axis('off')
 
-plt.show()
 
 #3) Entrainement du discriminateur
 
+#creation des modeles
+modelGen=Generator().to(device)
+modelDiscr=CNN().to(device)
 
+#Creation de la loss function
+lossFunction = nn.BCELoss()
+optimiseurDiscr = torch.optim.Adam(modelDiscr.parameters(), lr=0.001)
 
+iterationDiscr = 1000
+
+for i in range(iterationDiscr):
+
+    if i + batchsize > len(X_train_tensor):
+        break
+    
+    total_loss=0.0
+
+     # Initialisation des gradients
+    optimiseurDiscr.zero_grad()
+    
+    # Sélection d'un lot d'images réelles
+    real_img = X_train_tensor[i * batchsize:(i + 1) * batchsize].to(device)
+
+    #Creation des labels 
+    real_labels = torch.ones(batchsize,1).to(device)
+    fake_labels = torch.zeros(batchsize,1).to(device)
+
+    #passage dans le CNN des vrais images
+    prediction_real   = modelDiscr.forwardPropagation(real_img)
+    #On calcul la loss function uniquement sur la partie des images réels et on attribue un label reel
+    loss_real = lossFunction(prediction_real,real_labels)
+
+    #Generation d'une image fausse
+    noise = noise = torch.randn(batchsize,100,1,1).to(device) # 64 échantillons, 100 dimensions, 1x1 (h, w)
+    fake_img=modelGen(noise)
+    #passage de l'image fausse dans le discriminateur et on mets le fake label dessus
+    prediction_fake = modelDiscr.forwardPropagation(fake_img)
+    loss_fake = lossFunction(prediction_fake,fake_labels)
+
+    discrloss = loss_real+loss_fake #mesure du discriminateur pour l'optimiseur
+    discrloss.backward()
+    optimiseurDiscr.step()
+
+    total_loss += discrloss.item() 
+    print(total_loss)
+    
+    
 
 
 
